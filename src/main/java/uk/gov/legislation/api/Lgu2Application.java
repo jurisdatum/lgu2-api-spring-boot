fd64a7b2d0c3c66fb58b536d8c644efc682b9181
Jim Mangiafico <jim@jurisdatum.com>
2024-07-31 16:34:45 -0400

    initial commit

---
+++ src/main/java/uk/gov/legislation/api/Lgu2Application.java
@@ -0,0 +1,13 @@
+package uk.gov.legislation.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lgu2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lgu2Application.class, args);
+	}
+
+}
---
+++ src/test/java/uk/gov/legislation/api/Lgu2ApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gov.legislation.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lgu2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
