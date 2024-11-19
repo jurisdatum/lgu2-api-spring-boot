09b6f5b33bb52bdc55633ff6289dae1563afd0f6
Jim Mangiafico <jim@jurisdatum.com>
2024-11-19 12:52:10 -0800

    new timestamp for each error response

--- src/main/java/uk/gov/legislation/exceptions/ErrorResponse.java
+++ src/main/java/uk/gov/legislation/exceptions/ErrorResponse.java
@@ -1,19 +1,21 @@
 package uk.gov.legislation.exceptions;
 
-
 import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 public class ErrorResponse {
+
     private final String status;
     private final String message;
     private final String timestamp;
 
-    public ErrorResponse(String status, String message, String timestamp) {
+    public ErrorResponse(String status, String message) {
         this.status = status;
         this.message = message;
-        this.timestamp = timestamp;
+        this.timestamp = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);;
     }
-
 
 }
--- src/main/java/uk/gov/legislation/exceptions/GlobalExceptionHandler.java
+++ src/main/java/uk/gov/legislation/exceptions/GlobalExceptionHandler.java
@@ -7,19 +7,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import uk.gov.legislation.data.marklogic.NoDocumentException;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    String isoTimestamp = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
 
     @ExceptionHandler(NoDocumentException.class)
     public ResponseEntity <ErrorResponse> handleNoDocumentException(NoDocumentException ex) {
         ErrorResponse errorResponse = new ErrorResponse(
                 "Not Found",
-                "Document not found: " + ex.getMessage(),
-                isoTimestamp
+                "Document not found: " + ex.getMessage()
         );
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
@@ -28,8 +24,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleAknTransformationException(TransformationException ex) {
         ErrorResponse errorResponse = new ErrorResponse(
                 "Transformation Error",
-                "Transformation failed: " + ex.getMessage(),
-                isoTimestamp
+                "Transformation failed: " + ex.getMessage()
         );
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
@@ -38,8 +33,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleXSLTCompilationException(XSLTCompilationException ex) {
         ErrorResponse errorResponse = new ErrorResponse(
                 "Error compiling XSLT",
-                "XSLTCompilationException failed: " + ex.getMessage(),
-                isoTimestamp
+                "XSLTCompilationException failed: " + ex.getMessage()
         );
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
@@ -48,8 +42,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleInvalidURISyntaxException(InvalidURISyntaxException ex) {
         ErrorResponse errorResponse = new ErrorResponse(
                 "InvalidURISyntax_Error",
-                "InvalidURISyntax: " + ex.getMessage(),
-                isoTimestamp
+                "InvalidURISyntax: " + ex.getMessage()
         );
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
@@ -57,9 +50,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MarkLogicRequestException.class)
     public ResponseEntity<ErrorResponse> handleMarkLogicRequestException(MarkLogicRequestException ex) {
         ErrorResponse errorResponse = new ErrorResponse(
-                "Mark-Logic Request Error",
-                "Mark-Logic Error: " + ex.getMessage(),
-                isoTimestamp
+                "MarkLogic Request Error",
+                "MarkLogic Error: " + ex.getMessage()
         );
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
@@ -68,8 +60,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleIOException(IOException ex) {
         ErrorResponse errorResponse = new ErrorResponse(
                 "IOException_Error",
-                "An IO error occurred: " + ex.getMessage(),
-                isoTimestamp
+                "An IO error occurred: " + ex.getMessage()
         );
         return new ResponseEntity<>(errorResponse, HttpStatus.SERVICE_UNAVAILABLE);
     }
@@ -79,19 +70,16 @@ public class GlobalExceptionHandler {
         Thread.currentThread().interrupt();
         ErrorResponse errorResponse = new ErrorResponse(
                 "InterruptedException_Error",
-                "The request was interrupted: " + ex.getMessage(),
-                isoTimestamp
+                "The request was interrupted: " + ex.getMessage()
         );
         return new ResponseEntity<>(errorResponse, HttpStatus.SERVICE_UNAVAILABLE);
     }
 
     @ExceptionHandler(DocumentFetchException.class)
     public ResponseEntity<Object> handleDocumentFetchException(DocumentFetchException ex) {
-
         ErrorResponse errorResponse = new ErrorResponse(
                 "Document fetch failed",
-                "Error-Document fetch failed: " + ex.getMessage(),
-                isoTimestamp
+                "Document fetch failed: " + ex.getMessage()
         );
         return new ResponseEntity<>(errorResponse, HttpStatus.SERVICE_UNAVAILABLE);
     }
