abe61b9a8911ca2bede21b1b865b319237d20af9
AasifTolasabGulbarga <aasif.gulbarga@williamslea.com>
2025-02-12 11:37:31 +0530

    updated long name string of types

--- src/main/java/uk/gov/legislation/util/Type.java
+++ src/main/java/uk/gov/legislation/util/Type.java
@@ -3,45 +3,45 @@ package uk.gov.legislation.util;
 public enum Type {
 
     // United Kingdom Acts
-    UKPGA("United Kingdom Public General Act", Category.Primary, Country.UK),
-    UKLA("United Kingdom Local Act", Category.Primary, Country.UK),
-    UKPPA("United Kingdom Private or Personal Act", Category.Primary, Country.UK),
-    UKSI("United Kingdom Statutory Instrument", Category.Secondary, Country.UK),
-    UKMD("United Kingdom Ministerial Direction", Category.Secondary, Country.UK),
-    UKMO("United Kingdom Ministerial Order", Category.Secondary, Country.UK),
-    UKSRO("United Kingdom Statutory Rule or Order", Category.Secondary, Country.UK),
-    UKDSI("United Kingdom Draft Statutory Instrument", Category.Secondary, Country.UK),
+    UKPGA("UnitedKingdomPublicGeneralAct", Category.Primary, Country.UK),
+    UKLA("UnitedKingdomLocalAct", Category.Primary, Country.UK),
+    UKPPA("UnitedKingdomPrivateOrPersonalAct", Category.Primary, Country.UK),
+    UKSI("UnitedKingdomStatutoryInstrument", Category.Secondary, Country.UK),
+    UKMD("UnitedKingdomMinisterialDirection", Category.Secondary, Country.UK),
+    UKMO("UnitedKingdomMinisterialOrder", Category.Secondary, Country.UK),
+    UKSRO("UnitedKingdomStatutoryRuleOrOrder", Category.Secondary, Country.UK),
+    UKDSI("UnitedKingdomDraftStatutoryInstrument", Category.Secondary, Country.UK),
 
     // Wales Acts
-    ANAW("Welsh National Assembly Act", Category.Primary, Country.WALES),
-    ASC("Welsh Parliament Act", Category.Primary, Country.WALES),
-    MWA("Welsh Assembly Measure", Category.Primary, Country.WALES),
-    WSI("Welsh Statutory Instrument", Category.Secondary, Country.WALES),
+    ASC("WelshParliamentAct", Category.Primary, Country.WALES),
+    WSI("WelshStatutoryInstrument", Category.Secondary, Country.WALES),
+    ANAW("WelshNationalAssemblyAct", Category.Primary, Country.WALES),
+    MWA("WelshAssemblyMeasure", Category.Primary, Country.WALES),
 
     // Scotland Acts
-    ASP("Scottish Act", Category.Primary, Country.SCOTLAND),
-    AOSP("Scottish Old Act", Category.Primary, Country.SCOTLAND),
-    SSI("Scottish Statutory Instrument", Category.Secondary, Country.SCOTLAND),
-    SDSI("Scottish Draft Statutory Instrument", Category.Secondary, Country.SCOTLAND),
+    ASP("ScottishAct", Category.Primary, Country.SCOTLAND),
+    AOSP("ScottishOldAct", Category.Primary, Country.SCOTLAND),
+    SSI("ScottishStatutoryInstrument", Category.Secondary, Country.SCOTLAND),
+    SDSI("ScottishDraftStatutoryInstrument", Category.Secondary, Country.SCOTLAND),
 
     // Northern Ireland Acts
-    NIA("Northern Ireland Act", Category.Primary, Country.NORTHERN_IRELAND),
-    AIP("Ireland Act", Category.Primary, Country.NORTHERN_IRELAND),
-    NISR("Northern Ireland Statutory Rule", Category.Secondary, Country.NORTHERN_IRELAND),
-    NISI("Northern Ireland Order in Council", Category.Secondary, Country.NORTHERN_IRELAND),
-    MNIA("Northern Ireland Assembly Measure", Category.Primary, Country.NORTHERN_IRELAND),
-    APNI("Northern Ireland Parliament Act", Category.Primary, Country.NORTHERN_IRELAND),
-    NISRO("Northern Ireland Statutory Rule or Order", Category.Secondary, Country.NORTHERN_IRELAND),
-    NIDSR("Northern Ireland Draft Statutory Rule", Category.Secondary, Country.NORTHERN_IRELAND),
+    NIA("NorthernIrelandAct", Category.Primary, Country.NORTHERN_IRELAND),
+    AIP("IrelandAct", Category.Primary, Country.NORTHERN_IRELAND),
+    NISR("NorthernIrelandStatutoryRule", Category.Secondary, Country.NORTHERN_IRELAND),
+    NISI("NorthernIrelandOrderInCouncil", Category.Secondary, Country.NORTHERN_IRELAND),
+    MNIA("NorthernIrelandAssemblyMeasure", Category.Primary, Country.NORTHERN_IRELAND),
+    APNI("NorthernIrelandParliamentAct", Category.Primary, Country.NORTHERN_IRELAND),
+    NISRO("NorthernIrelandStatutoryRuleOrOrder", Category.Secondary, Country.NORTHERN_IRELAND),
+    NIDSR("NorthernIrelandDraftStatutoryRule", Category.Secondary, Country.NORTHERN_IRELAND),
 
     // Great Britain Acts
-    APGB("Great Britain Act", Category.Primary, Country.GREAT_BRITAIN),
-    GBLA("Great Britain Local Act", Category.Primary, Country.GREAT_BRITAIN),
+    APGB("GreatBritainAct", Category.Primary, Country.GREAT_BRITAIN),
+    GBLA("GreatBritainLocalAct", Category.Primary, Country.GREAT_BRITAIN),
 
     // England Acts
-    UKCM("United Kingdom Church Measure", Category.Primary, Country.ENGLAND),
-    UKCI("United Kingdom Church Instrument", Category.Secondary, Country.ENGLAND),
-    AEP("England Act", Category.Primary, Country.ENGLAND);
+    UKCM("UnitedKingdomChurchMeasure", Category.Primary, Country.ENGLAND),
+    UKCI("UnitedKingdomChurchInstrument", Category.Secondary, Country.ENGLAND),
+    AEP("EnglandAct", Category.Primary, Country.ENGLAND);
 
     public enum Category { Primary, Secondary }
 
