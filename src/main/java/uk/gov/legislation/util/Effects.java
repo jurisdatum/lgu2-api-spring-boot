0c303d17a4284711eaf17b2dcd801bb4984cc2b2
Jim Mangiafico <jim@jurisdatum.com>
2025-02-19 16:42:48 -0500

    prepare to filter by section ranges

--- src/main/java/uk/gov/legislation/util/Effects.java
+++ src/main/java/uk/gov/legislation/util/Effects.java
@@ -8,7 +8,6 @@ import uk.gov.legislation.transform.simple.effects.Effect;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
-import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class Effects {
@@ -32,27 +31,49 @@ public class Effects {
 
     /* sections */
 
-    static final Predicate<RichTextNode> isSection = node -> node instanceof RichTextNode.Section;
+    static List<RichTextNode.Section> getAffectedSections(Effect effect) {
+        return Effects.flatten(effect.affectedProvisions)
+            .filter(node -> node instanceof RichTextNode.Section)
+            .map(RichTextNode.Section.class::cast)
+            .toList();
+    }
 
-    static final Predicate<Effect> isPlainText = effect -> effect.affectedProvisions.stream()
-        .noneMatch(isSection.or(node -> node instanceof RichTextNode.Range));
+    static List<RichTextNode.Range> getAffectedRanges(Effect effect) {
+        return effect.affectedProvisions.stream()
+            .filter(node -> node instanceof RichTextNode.Range)
+            .map(RichTextNode.Range.class::cast)
+            .toList();
+    }
+
+    static boolean rangeIncludesAny(RichTextNode.Range range, Set<String> ids) {
+        return ids.stream().anyMatch(id -> rangeIncludes(range, id));
+    }
+    static boolean rangeIncludes(RichTextNode.Range range, String id) {
+        if (range.start.equals(id))
+            return true;
+        if (range.end.equals(id))
+            return true;
+        return false;
+    }
+
+    static boolean relatesTo(Effect effect, Set<String> ids, boolean includeWhole) {
+        List<RichTextNode.Section> sections = getAffectedSections(effect);
+        if (sections.isEmpty())
+            return includeWhole;
+        if (sections.stream().anyMatch(n -> ids.contains(n.ref)))
+            return true;
+        List<RichTextNode.Range> ranges = getAffectedRanges(effect);
+        return ranges.stream().anyMatch(range -> rangeIncludesAny(range, ids));
+    }
 
     /**
-     *
-     * @param effects
-     * @param ids the fragment ids to be retained
-     * @param includeWhole retain those targeting no particular section
-     * @return
+     @param effects all the effects
+     @param ids the fragment ids to be retained
+     @param includeWhole retain those targeting no particular section
+     @return a list of filtered effects
      */
     public static List<Effect> removeThoseWithNoRelevantSection(List<Effect> effects, Set<String> ids, boolean includeWhole) {
-        Predicate<Effect> someSectionsAreRelevant = effect -> Effects.flatten(effect.affectedProvisions)
-            .filter(isSection)
-            .map(RichTextNode.Section.class::cast)
-            .anyMatch(n -> ids.contains(n.ref));
-        if (includeWhole)
-            return effects.stream().filter(isPlainText.or(someSectionsAreRelevant)).toList();
-        else
-            return effects.stream().filter(someSectionsAreRelevant).toList();
+        return effects.stream().filter(effect -> relatesTo(effect, ids, includeWhole)).toList();
     }
 
 }
