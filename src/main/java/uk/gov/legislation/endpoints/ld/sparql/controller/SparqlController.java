0481015e5a18e2f5e2825ad61a4ab300cd5c1d7d
Jim Mangiafico <jim@jurisdatum.com>
2025-03-14 15:05:25 -0400

    rework SPARQL queries

--- src/main/java/uk/gov/legislation/endpoints/ld/sparql/controller/SparqlController.java
+++ src/main/java/uk/gov/legislation/endpoints/ld/sparql/controller/SparqlController.java
@@ -1,1 +1,36 @@
-package uk.gov.legislation.endpoints.ld.sparql.controller;import jakarta.servlet.http.HttpServletRequest;import org.springframework.http.ResponseEntity;import org.springframework.web.bind.annotation.RestController;import uk.gov.legislation.data.virtuoso.Virtuoso;import uk.gov.legislation.endpoints.ld.sparql.api.SparqlApi;import java.io.IOException;@RestControllerpublic class SparqlController implements SparqlApi {    private final Virtuoso virtuoso;    public SparqlController(Virtuoso virtuoso) {        this.virtuoso = virtuoso;    }    @Override    public ResponseEntity <String> sparql(String query,  HttpServletRequest request) throws IOException, InterruptedException {        boolean isGetRequest = request.getMethod().equalsIgnoreCase("GET");        String response = virtuoso.query(query, isGetRequest,request);        return ResponseEntity.ok().body(response);    }}
+package uk.gov.legislation.endpoints.ld.sparql.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.legislation.data.virtuoso.Virtuoso;
+import uk.gov.legislation.endpoints.ld.sparql.api.SparqlApi;
+
+@RestController
+public class SparqlController implements SparqlApi {
+
+    private final Virtuoso virtuoso;
+
+    public SparqlController(Virtuoso virtuoso) {
+        this.virtuoso = virtuoso;
+    }
+
+    @Override
+    public ResponseEntity<String> sparql(String query, String accept) throws Exception {
+        String format = switch (accept) {
+            case "application/rdf+xml", "application/sparql-results+json",
+                 "application/sparql-results+xml",
+                 "text/csv", "text/plain", "text/turtle" -> accept;
+            case "application/xml" -> "application/sparql-results+xml";
+            default -> "application/sparql-results+json";
+        };
+        String response;
+        try {
+            response = virtuoso.query(query, format);
+        } catch (IllegalArgumentException e) {
+            throw new HttpMediaTypeNotAcceptableException(format);
+        }
+        return ResponseEntity.ok().body(response);
+    }
+
+}
