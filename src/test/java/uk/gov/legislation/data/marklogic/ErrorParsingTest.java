jurisdatum/lgu2-api-spring-boot

commit b836acfc64e3a48457bda4f206afe7d8594d3084

reuse error mapper across requests

src/main/java/uk/gov/legislation/data/marklogic/Error.java
@@ -1,6 +1,7 @@
 package uk.gov.legislation.data.marklogic;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
@@ -13,9 +14,11 @@ import java.util.List;
 public class Error {
 
     public static Error parse(String xml) throws JsonProcessingException {
-        XmlMapper mapper = new XmlMapper();
-        return mapper.readValue(xml, Error.class);
+        return MAPPER.readValue(xml, Error.class);
     }
+
+    private static final XmlMapper MAPPER = (XmlMapper) new XmlMapper()
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
     @JacksonXmlProperty(localName = "status-code")
     public int statusCode;

commit e5af79c113a8bbee71a1438937dae9728710f85c

fix error parsing bug, introduced in commit b836acf

src/main/java/uk/gov/legislation/data/marklogic/Error.java
@@ -18,7 +18,7 @@ public class Error {
     }
 
     private static final XmlMapper MAPPER = (XmlMapper) new XmlMapper()
-        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
 
     @JacksonXmlProperty(localName = "status-code")
     public int statusCode;

src/test/java/uk/gov/legislation/data/marklogic/ErrorParsingTest.java
@@ -1,0 +1,39 @@
+package uk.gov.legislation.data.marklogic;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ErrorParsingTest {
+
+    @Test
+    @DisplayName("Error.parse succeeds on MarkLogic <error> payloads")
+    void parseValidErrorXml() {
+        String xml = """
+            <error xmlns="">
+                <status-code>404</status-code>
+                <message>The item of legislation you've requested isn't available on this site and we can't find any references to it. Perhaps the link is slightly wrong.</message>
+                <link href="http://www.legislation.gov.uk/uksi/2025">Browse for other legislation of this type from 2025</link><link href="http://www.legislation.gov.uk/uksi">Browse for other legislation of this type</link>
+            </error>
+            """;
+
+        assertDoesNotThrow(() -> Error.parse(xml));
+    }
+
+    @Test
+    @DisplayName("Error.parse rejects CLML responses")
+    void parseRejectsClmlXml() throws IOException {
+        ClassPathResource resource = new ClassPathResource("ukpga_2023_29/ukpga-2023-29-2024-11-01.xml");
+        String xml = resource.getContentAsString(StandardCharsets.UTF_8);
+
+        assertThrows(JsonProcessingException.class, () -> Error.parse(xml));
+    }
+
+}