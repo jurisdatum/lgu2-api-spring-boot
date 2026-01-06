4cba378dec2aced987988d9d2e0e17dd04f65770
AasifTolasabGulbarga <aasif.gulbarga@williamslea.com>
2026-01-06 19:34:02 +0530

    corrected import statement of number and year annotation

--- src/main/java/uk/gov/legislation/endpoints/search/SearchParameters.java
+++ src/main/java/uk/gov/legislation/endpoints/search/SearchParameters.java
@@ -3,7 +3,8 @@ package uk.gov.legislation.endpoints.search;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
-import uk.gov.legislation.api.parameters.Sort;
+import uk.gov.legislation.api.parameters.*;
+import uk.gov.legislation.api.parameters.Number;
 import uk.gov.legislation.data.marklogic.search.Parameters;
 import uk.gov.legislation.util.Extent;
 
