7888979f894bb2d7dd68516d2e691a1bc98c64e9
Jim Mangiafico <jim@jurisdatum.com>
2026-03-17 15:43:58 -0700

    add title and modified fields to AssocMeta
    Both fields are available in the MarkLogic response but were not being
    mapped through to the API. title is constructed by the XQuery from the
    associated legislation's title; modified is standard Dublin Core.

--- src/main/java/uk/gov/legislation/api/responses/meta/AssocMeta.java
+++ src/main/java/uk/gov/legislation/api/responses/meta/AssocMeta.java
@@ -2,9 +2,16 @@ package uk.gov.legislation.api.responses.meta;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class AssocMeta extends MetaCore {
+
+    @Schema
+    public String title;
+
+    @Schema
+    public LocalDate modified;
 
     @Schema
     public MetaCore associatedWith;
