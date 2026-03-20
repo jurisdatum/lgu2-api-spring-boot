84d81bb0fd334e74b21cd0a8b2b53c2109a81d2f
AasifTolasabGulbarga <aasif.gulbarga@williamslea.com>
2026-03-20 23:34:56 +0530

    remove EffectsSortConverter for making separate

--- src/main/java/uk/gov/legislation/api/parameters/SortConverter.java
+++ src/main/java/uk/gov/legislation/api/parameters/SortConverter.java
@@ -2,7 +2,6 @@ package uk.gov.legislation.api.parameters;
 
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
-import uk.gov.legislation.data.marklogic.changes.Parameters;
 import uk.gov.legislation.data.marklogic.search.Parameters.Sort;
 
 @Component
@@ -12,11 +11,4 @@ public class SortConverter implements Converter<String, Sort> {
     public Sort convert(String sort) {
         return Sort.fromValue(sort);
     }
-     public static class EffectsSortConverter implements Converter<String, Parameters.EffectsSort> {
-
-         @Override
-         public Parameters.EffectsSort convert(String source) {
-             return Parameters.EffectsSort.fromValue(source);
-         }
-     }
 }
